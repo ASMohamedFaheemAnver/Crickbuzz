47adf4e9bc14d071c80381c2b21b52a647c4f861
ubunturifa <jstrfaheem065@gmail.com>
2019-07-06 18:13:15 +0530

    Codes modified,

--- src/main/java/com/company/crickbuzzpage/DefaultCrickbuzzPage.java
+++ src/main/java/com/company/crickbuzzpage/DefaultCrickbuzzPage.java
@@ -5,15 +5,9 @@ package com.company.crickbuzzpage;
 import com.company.operation.VoidOperation;
 import com.company.operation.OperationFactory;
 
-import javax.swing.JFrame;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
@@ -35,21 +29,31 @@ public class DefaultCrickbuzzPage implements CrickbuzzPage{
 
     @Override
     public void display() {
+        defaultFrame.setContentPane(new JLabel(new ImageIcon("/home/ubunturifa/Downloads/cricket-opener-Cricket-wallpapers.jpg")));
+
         userGreeting.setText("WELCOME TO THE AWESOME CRICKBUZZ!");
         searchInstruction.setText("Enter Match ID : ");
         userGreeting.setBounds(650, 0, 700, 100);
-        searchInstruction.setBounds(500, 250, 150, 40);
-        searchText.setBounds(630, 250, 100, 40);
-        searchButton.setBounds(1350, 250, 100, 40);
-        firstInnings.setBounds(770, 250, 150, 40);
-        secondInnings.setBounds(970, 250, 150, 40);
-        inningsOverview.setBounds(1170, 250, 150, 40);
+        searchInstruction.setBounds(50, 250, 150, 40);
+        searchText.setBounds(200, 250, 100, 40);
+        searchButton.setBounds(960, 250, 100, 40);
+        firstInnings.setBounds(340, 250, 140, 40);
+        secondInnings.setBounds(530, 250, 160, 40);
+        inningsOverview.setBounds(740, 250, 180, 40);
 
         inningsOverview.setActionCommand("Innings Overview");
         firstInnings.setActionCommand("First Innings");
         secondInnings.setActionCommand("Second Innings");
 
         userGreeting.setFont(new Font("Serif", Font.BOLD, 25));
+
+
+        searchInstruction.setFont(new Font("Serif", Font.BOLD, 15));
+        searchText.setFont(new Font("Serif", Font.BOLD, 15));
+        searchButton.setFont(new Font("Serif", Font.BOLD, 15));
+        firstInnings.setFont(new Font("Serif", Font.BOLD, 15));
+        secondInnings.setFont(new Font("Serif", Font.BOLD, 15));
+        inningsOverview.setFont(new Font("Serif", Font.BOLD, 15));
 
         selectionInnings.add(firstInnings);
         selectionInnings.add(secondInnings);
