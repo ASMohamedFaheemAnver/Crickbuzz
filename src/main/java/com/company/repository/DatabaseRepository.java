0587cf504dd3f1e2452226e51eb79e753424d2d6
ubunturifa <jstrfaheem065@gmail.com>
2019-07-06 15:35:35 +0530

    Codes modified,

--- src/main/java/com/company/repository/DatabaseRepository.java
+++ src/main/java/com/company/repository/DatabaseRepository.java
@@ -6,9 +6,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-
-import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 
 public class DatabaseRepository implements Repository{
@@ -23,10 +20,10 @@ public class DatabaseRepository implements Repository{
             sqlstat = sqlcon.prepareStatement(quary);
             sqlres = sqlstat.executeQuery();
         } catch (SQLException e) {
-            LOGGER.log(Level.INFO, "Got an exception.", e);
+            e.getMessage();
             JOptionPane.showMessageDialog(null, "THERE WAS AN SQL EXCEPTION!");
         }catch (Exception e){
-            LOGGER.log(Level.INFO, "Got an exception.", e);
+            e.getMessage();
             JOptionPane.showMessageDialog(null, "   CAN'T CONNECT TO THE DATABASE!");
         }
         return sqlres;
